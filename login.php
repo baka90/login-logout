85d3809531455bf54e15b28832d8313f4480d957
Baka <ahmed_albaka@hotmail.com>
2017-06-24 22:00:50 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,35 +1,45 @@
 <?php
+//ob_start();
 
-class Login {
-	public static function isLoggedIn() {
-		if (isset($_COOKIE['SNID'])) {
+include('classes/DB.php');
 
-			if (DB::query('SELECT user_id FROM login_tokens WHERE token=:token', array(':token'=>sha1($_COOKIE['SNID'])))) {
+if (isset($_POST['login'])) {
 
-				$userid = DB::query('SELECT user_id FROM login_tokens WHERE token=:token', array(':token'=>sha1($_COOKIE['SNID'])))[0]['user_id'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
-				if(isset($_COOKIE['SNID_'])) {
+	if ( DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username) )) {
 
-					return $userid;
+		if (password_verify($password, DB::query('SELECT password FROM users WHERE username=:username', array(':username'=>$username))[0]['password'])) {
 
-				} else {
+			//Make two cookies, the second one to check the extended time
+			$cstrong = true;
+			$token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+			$user_id = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$username))[0]['id'];
+			DB::query('INSERT INTO login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$user_id));
 
-					// If there is no second check cookie, resend the first coockie to extend logged in period
-					$cstrong = true;
-					$token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
-					DB::query('INSERT INTO login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$userid));
-					DB::query('DELETE FROM login_tokens WHERE token=:token', array(':token'=>sha1($_COOKIE['SNID'])));
+			setcookie("SNID", $token, time() + 60 * 60, '/', NULL, NULL, TRUE);
+			setcookie("SNID_", '1', time() + 60 * 5, '/', NULL, NULL, TRUE);
 
-					setcookie("SNID", $token, time() + 60 * 60, '/', NULL, NULL, TRUE);
-					setcookie("SNID_", '1', time() + 60 * 5, '/', NULL, NULL, TRUE);
+			echo 'Welkom ', $username;
 
-					return $userid;
+		} else {
 
-				}
-			}
+			echo 'Gebruikersnaam met wachtwoord combinatie onbekend';
 		}
-		return false;
+
+	} else {
+
+		echo 'Gebruikersnaam met wachtwoord combinatie onbekend';
 	}
 }
 
 ?>
+
+<h1>Inloggen</h1>
+<form action="login.php" method="post">
+	<input type="text" name="username" value="" placeholder="username..."> <p />
+	<input type="password" name="password" value="" placeholder="password..."> <p />
+	<input type="submit" name="login" value="Log in">
+	<p id="testje"></p>
+</form>
