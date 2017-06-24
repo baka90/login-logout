6e4f9c5b237242fada1d53c1590c6ff12c9f7ceb
Baka <ahmed_albaka@hotmail.com>
2017-06-24 22:03:09 +0200

    Create DB.php

---
+++ classes/DB.php
@@ -0,0 +1,27 @@
+<?php 
+
+class DB {
+ 	// Connect to database
+	private static function connect() {
+
+		$pdo = new PDO('mysql:host=localhost;dbname=SocialNetwork;charset=utf8', 'drupaluser', '');
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $pdo;
+
+	}
+ 	// Interaction with database
+	public static function query($query, $params = array()) {
+
+		// Prepare query with variables (Anti SQL injection)
+		$statement = self::connect()->prepare($query);
+
+		// Execute account while defining variables 
+		$statement->execute($params);
+
+		// Get only the select queries and return fetched data
+		if (explode(' ', $query)[0] == 'SELECT') {
+			$data = $statement->fetchAll();
+			return $data;
+		}
+	}	
+}
